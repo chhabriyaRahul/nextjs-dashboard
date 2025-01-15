1ae44488371f045e44fc7a9c92a624346c20a97f
chhabriyaRahul <116173046+chhabriyaRahul@users.noreply.github.com>
2025-01-15 15:36:42 +0530

    Data preparing
    Extracted the data from formData object and Validated it using 'zod'.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,23 @@
 'use server';
 
-export async function createInvoice(formData: FormData){
+import {z} from 'zod';//a TypeScript validation library
 
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending','paid']),
+    date: z.string()
+});
+
+const CreateInvoice = FormSchema.omit({id:true, date:true});
+
+export async function createInvoice(formData: FormData){
+    const {customerId, amount, status} = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+    const amountInCents = amount * 100;//to eliminate JS floating-point errors
+    const date = new Date().toISOString().split('T')[0];
 }
