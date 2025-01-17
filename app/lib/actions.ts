1b60f5f476d342c342e3cce622374489a0829c52
chhabriyaRahul <116173046+chhabriyaRahul@users.noreply.github.com>
2025-01-17 15:45:47 +0530

    Server actions with try/catch
    Surrounded server actions with try/catch block and also implemented server action for deleting an invoice.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({id:true, date:true});
 const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
+
 export async function createInvoice(formData: FormData){
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -25,10 +26,14 @@ export async function createInvoice(formData: FormData){
     const amountInCents = amount * 100;//to eliminate JS floating-point errors
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    }catch (error) {
+        console.log(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -42,11 +47,23 @@ export async function updateInvoice(id: string, formData: FormData){
     });
     const amountInCents = amount * 100;
 
+    try {
     await sql`
     UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
     `;
+    }catch (error){
+        console.log(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string){
+    throw new Error('Failed to delete invoice');//Temporary disabling delete property
+
+    //Unreachable code block
+    await sql `DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
