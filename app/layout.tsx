98b15d58d7e8d94b3b124905e79fef561d4378a3
chhabriyaRahul <116173046+chhabriyaRahul@users.noreply.github.com>
2025-04-25 14:59:05 +0530

    Metadata
    Added metadata for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import {inter} from "@/app/ui/fonts";
+import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Financial Dashboard',
+    default: 'Financial Dashboard',
+  },
+  description: 'Demo NextJs Project',
+  metadataBase: new URL('https://nextjs-dashboard-sable-seven-44.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
